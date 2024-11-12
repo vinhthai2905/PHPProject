585b7ae7a945292eb57a0c44ad1aa29a209f0b87
Thái Bùi <vinhthai2905@aol.com>
2024-11-12 22:22:42 +0700

    adding more for table in migration

--- database/migrations/2024_11_12_150555_create_posts_table.php
+++ database/migrations/2024_11_12_150555_create_posts_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->mediumText('body');
+            $table->date('date');
             $table->timestamps();
            
         });
